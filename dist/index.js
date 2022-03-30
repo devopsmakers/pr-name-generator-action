d0aaba3fed9f190875adf089c8aa77f33fab8667
Tim Birkett <tim.birkett@sainsburys.co.uk>
2022-03-30 15:37:50 +0100

    chore: stringify issue object

--- dist/index.js
+++ dist/index.js
@@ -8488,17 +8488,19 @@ const {uniqueNamesGenerator,
 try {
   const context = github.context;
 
+  const seed = JSON.stringify(context.issue);
+
   const config = {
     dictionaries: [adjectives, colors, animals, names],
     separator: '-',
     length: 2,
     style: 'lowerCase',
-    seed: `${context.issue}`,
+    seed: seed,
   };
 
   const output = uniqueNamesGenerator(config);
 
-  core.info(`Seed: ${context.issue}`);
+  core.info(`Seed: ${seed}`);
   core.info(`Unique name: ${output}`);
   core.setOutput('name', output);
 } catch (error) {
--- index.test.js
+++ index.test.js
@@ -6,5 +6,5 @@ test('test runs', () => {
   process.env['GITHUB_EVENT_PATH'] = './fixtures/event.json';
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-  expect(result).toContain('::set-output name=name::mute-moccasin');
+  expect(result).toContain('::set-output name=name::civilian-tan');
 });
